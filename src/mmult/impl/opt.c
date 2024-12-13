f8c0c85c076a8ac070abc01cf273cbd3de5b9c11
haithamswe <haitham.1419@outlook.sa>
2024-12-13 14:35:27 +0300

    adding the optimized impl

--- src/mmult/impl/opt.c
+++ src/mmult/impl/opt.c
@@ -1,26 +1,45 @@
-/* opt.c
- *
- * Author:
- * Date  :
- *
- *  Description
- */
-
-/* Standard C includes */
-#include <stdlib.h>
-
-/* Include common headers */
-#include "common/macros.h"
 #include "common/types.h"
-
-/* Include application-specific headers */
 #include "include/types.h"
+#include <stdlib.h>
+#include <stddef.h>
 
-/* Alternative Implementation */
-#pragma GCC push_options
-#pragma GCC optimize ("O1")
-void* impl_scalar_opt(void* args)
-{
-  return NULL;
+#ifndef MIN
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
+void* impl_scalar_opt(void* args, int block_size) {
+    args_t* arguments = (args_t*)args;
+
+    int* A = (int*)arguments->matrix_a;
+    int* B = (int*)arguments->matrix_b;
+    int* R = malloc(arguments->M * arguments->P * sizeof(int));
+
+    int M = arguments->M;
+    int N = arguments->N;
+    int P = arguments->P;
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < P; j++) {
+            R[i * P + j] = 0;
+        }
+    }
+
+    for (int ii = 0; ii < M; ii += block_size) {
+        for (int jj = 0; jj < P; jj += block_size) {
+            for (int kk = 0; kk < N; kk += block_size) {
+                for (int i = ii; i < MIN(ii + block_size, M); i++) {
+                    for (int j = jj; j < MIN(jj + block_size, P); j++) {
+                        int sum = 0;
+                        for (int k = kk; k < MIN(kk + block_size, N); k++) {
+                            sum += A[i * N + k] * B[k * P + j];
+                        }
+                        R[i * P + j] += sum;
+                    }
+                }
+            }
+        }
+    }
+
+    arguments->result = R;
+    return NULL;
 }
-#pragma GCC pop_options
--- src/mmult/impl/opt.h
+++ src/mmult/impl/opt.h
@@ -1,15 +1,7 @@
-/* opt.h
- *
- * Author: Khalid Al-Hawaj
- * Date  : 13 Nov. 2023
- *
- * Header for scalar optimized function.
- */
-
 #ifndef __IMPL_OPT_H_
 #define __IMPL_OPT_H_
 
 /* Function declaration */
-void* impl_scalar_opt(void* args);
+void* impl_scalar_opt(void* args, int block_size);
 
 #endif //__IMPL_OPT_H_
